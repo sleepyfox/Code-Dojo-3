f209d6b84cb35a8304fbdab0ff52586b8825d883
Fox <sleepyfox@gmail.com>
2015-03-10 16:19:21 +0000

    added tests for tMax and tMin

--- python/test-temperature-spread.py
+++ python/test-temperature-spread.py
@@ -1,7 +1,8 @@
 "A test suite for a temperature spread calculator"
 
 import unittest
-
+import re
+#    1948   5   18.1     6.9    ---     57.0    ---
 # class OutOfRangeError: pass
 # class LookupError: pass
 
@@ -11,6 +12,20 @@ class TemperatureAnalyser:
 
 	def countLines(self):
 		return len(TemperatureAnalyser.lines)
+
+	def tMax(self, line):
+		THIRD_MATCH = 2
+		DIGITS_REGEXP = "[\d\.]+"
+		digits = re.compile(DIGITS_REGEXP)
+		results = digits.findall(line)
+		return float(results[THIRD_MATCH])
+
+	def tMin(self, line):
+		FOURTH_MATCH = 3
+		DIGITS_REGEXP = "[\d\.]+"
+		digits = re.compile(DIGITS_REGEXP)
+		results = digits.findall(line)
+		return float(results[FOURTH_MATCH])
 
 	def calculate_spread(self, first, second):
 		return first - second
@@ -30,6 +45,16 @@ class testTemperatureAnalyser(unittest.TestCase):
 	def testSpreadPositive(self):
 		"The spread between 4 and 1 is 3"
 		self.assertEqual(self.temps.calculate_spread(4,1), 3)
+
+	def testExtractHighTemp(self):
+		"Test that high temp from string is 18.1"
+		line = "   1948   5   18.1     6.9    ---     57.0    ---"
+		self.assertEqual(self.temps.tMax(line), 18.1)
+
+	def testExtractLowTemp(self):
+		"Test that the low temp from string is 6.9"
+		line = "   1948   5   18.1     6.9    ---     57.0    ---"
+		self.assertEqual(self.temps.tMin(line), 6.9)
 
 if __name__ == "__main__":
 	unittest.main()
