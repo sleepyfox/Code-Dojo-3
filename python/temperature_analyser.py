980d1af21f9e1145ffd05698ac3f3c88774da13e
Fox <sleepyfox@gmail.com>
2015-03-13 14:27:49 +0000

    extracted tests and implementation of temperature reading attributes to separate class

--- python/temperature_analyser.py
+++ python/temperature_analyser.py
@@ -2,15 +2,26 @@ import re
 
 class Temperature_Reading:
 	def __init__(self, line):
+		YEAR_FIELD = 0
+		MONTH_FIELD = 1
+		TMAX_FIELD = 2
+		TMIN_FIELD = 3
+		DIGITS_REGEXP = "\-?[\d\.]+\s"
+		digits = re.compile(DIGITS_REGEXP)
+		DATA_LINE_REGEXP = "\s+[12]\d{3}\s" # start line with a year
+		valid = re.compile(DATA_LINE_REGEXP)
 		if not line:
 			raise(ValueError)
 		else:
-			DATA_LINE_REGEXP = "\s+[12]\d{3}\s" # start line with a year
-			valid = re.compile(DATA_LINE_REGEXP)
 			if valid.match(line):
 				self.line = line
+				fields = digits.findall(line)
+				self.date = fields[YEAR_FIELD].strip() + "-" + fields[MONTH_FIELD].strip()
+				self.tMin = float(fields[TMIN_FIELD])
+				self.tMax = float(fields[TMAX_FIELD])
 			else:
 				raise(ValueError)
+
 
 class Temperature_Analyser:
 	def __init__(self):
@@ -20,6 +31,7 @@ class Temperature_Analyser:
 		DATA_LINE_REGEXP = "\s+[12]\d{3}\s" # start line with a year
 		valid = re.compile(DATA_LINE_REGEXP)
 		self.lines = [line.strip() for line in open(TEMPERATURE_FILE) if valid.match(line) ]
+		# self.readings =
 
 	def count_lines(self):
 		return len(self.lines)
--- python/test-temperature-spread.py
+++ python/test-temperature-spread.py
@@ -13,12 +13,30 @@ class test_Temperature_Reading(unittest.TestCase):
 		"Invalid data e.g. header line throws an exception"
 		with self.assertRaises(ValueError):
 			reading = ta.Temperature_Reading("Heathrow (London Airport)")
-			
-	# def test_read_date(self):
-	# 	"a temperature reading should have the correct date"
-	# 	line = "   1948   5   18.1     6.9    ---     57.0    ---"
-	# 	reading  =ta.Temperature_Reading(line)
-	# 	self.assertEqual(reading.date, "")
+
+	def test_extract_date(self):
+		"a temperature reading should have the correct date"
+		line = "   1948   5   18.1     6.9    ---     57.0    ---"
+		reading = ta.Temperature_Reading(line)
+		self.assertEqual(reading.date, "1948-5")
+
+	def test_extract_high_temp_from_line(self):
+		"Test that high temp from string is 18.1"
+		line = "   1948   5   18.1     6.9    ---     57.0    ---"
+		reading = ta.Temperature_Reading(line)
+		self.assertEqual(reading.tMax, 18.1)
+
+	def test_extract_low_temp_from_line(self):
+		"Test that the low temp from string is 6.9"
+		line = "   1948   5   18.1     6.9    ---     57.0    ---"
+		reading = ta.Temperature_Reading(line)
+		self.assertEqual(reading.tMin, 6.9)
+
+	def test_extract_negative_low_temp_from_line(self):
+		"Test we can extract a negative tMin of -1.3"
+		line = "   1991   2    5.4    -1.3      16    34.7    48.5"
+		reading = ta.Temperature_Reading(line)
+		self.assertEqual(reading.tMin, -1.3)
 
 class test_Temperature_Analyser(unittest.TestCase):
 	@classmethod
@@ -44,21 +62,6 @@ class test_Temperature_Analyser(unittest.TestCase):
 	def test_spread_with_negative_tMin(self):
 		"The spread between 2 and -1 should be 3"
 		self.assertEqual(self.temps.calculate_spread(2, -1), 3)
-
-	def test_extract_high_temp_from_line(self):
-		"Test that high temp from string is 18.1"
-		line = "   1948   5   18.1     6.9    ---     57.0    ---"
-		self.assertEqual(self.temps.tMax(line), 18.1)
-
-	def test_extract_low_temp_from_line(self):
-		"Test that the low temp from string is 6.9"
-		line = "   1948   5   18.1     6.9    ---     57.0    ---"
-		self.assertEqual(self.temps.tMin(line), 6.9)
-
-	def test_extract_negative_low_temp_from_line(self):
-		"Test we can extract a negative tMin of -1.3"
-		line = "   1991   2    5.4    -1.3      16    34.7    48.5"
-		self.assertEqual(self.temps.tMin(line), -1.3)
 
 	def test_calc_minimum_spread_from_list(self):
 		"Test that getting a minimum from a sequence of spreads"
