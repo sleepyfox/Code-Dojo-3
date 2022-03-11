ef515c80e0b827c49cbf2fa9369ab3cbbbfa00fb
Nigel Runnels-Moss <sleepyfox@gmail.com>
2022-03-11 18:47:04 +0000

    added a temperature spread object

--- nim/tests/munge_test.nim
+++ nim/tests/munge_test.nim
@@ -1,5 +1,8 @@
-import unittest
+import math
 import strutils
+import unittest
+
+doAssert almostEqual(PI, 3.14159265358979)
 
 const t = "   1948   1    8.9     3.3    ---     85.0    ---"
 
@@ -10,6 +13,12 @@ type
     max_temp: float
     min_temp: float
 
+type
+  TempSpread = object
+    year: int
+    month: int
+    temp_spread: float
+
 proc temp_reader(line: string): seq[string] =
   splitWhitespace(line)
 
@@ -18,6 +27,11 @@ proc strings_to_temp(ts: seq[string]): Temp =
        month: ts[1].parseInt(),
        max_temp: ts[2].parseFloat(),
        min_temp: ts[3].parseFloat())
+
+proc spread(t: Temp): TempSpread =
+  TempSpread(year: t.year,
+             month: t.month,
+             temp_spread: t.max_temp - t.min_temp)
 
 const tokens = temp_reader(t)
 const temp = strings_to_temp(tokens)
@@ -39,3 +53,7 @@ suite "a Temperature record object":
     check(temp.max_temp == 8.9)
   test "minimum recorded temperature is 3.3C":
     check(temp.min_temp == 3.3)
+
+suite "a temperature spread calculator":
+  test "should record a spread of 5.6C":
+    check(almostEqual(spread(temp).temp_spread, 5.6))
