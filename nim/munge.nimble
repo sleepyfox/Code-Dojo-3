94ac7cd45577b7a8bd265e85546b0b1931c3d642
@sleepyfox <sleepyfox@gmail.com>
2022-03-13 15:16:39 +0000

    moved implementation to a module

--- nim/munge.nimble
+++ nim/munge.nimble
@@ -7,8 +7,11 @@ license       = "CC-BY-NC-SA-4.0"
 srcDir        = "src"
 bin           = @["munge"]
 
-
-
 # Dependencies
 
-requires "nim >= 1.0.6"
+requires "nim >= 1.6.4"
+
+# Run tests
+
+task test, "Runs the test suite":
+  exec "nim c -r tests/munge_test"
--- nim/tests/munge_test.nim
+++ nim/tests/munge_test.nim
@@ -1,37 +1,9 @@
 import math
-import strutils
 import unittest
 
-doAssert almostEqual(PI, 3.14159265358979)
+import temp_spread
 
 const t = "   1948   1    8.9     3.3    ---     85.0    ---"
-
-type
-  Temp = object
-    year: int
-    month: int
-    max_temp: float
-    min_temp: float
-
-type
-  TempSpread = object
-    year: int
-    month: int
-    temp_spread: float
-
-proc temp_reader(line: string): seq[string] =
-  splitWhitespace(line)
-
-proc strings_to_temp(ts: seq[string]): Temp =
-  Temp(year: ts[0].parseInt(),
-       month: ts[1].parseInt(),
-       max_temp: ts[2].parseFloat(),
-       min_temp: ts[3].parseFloat())
-
-proc spread(t: Temp): TempSpread =
-  TempSpread(year: t.year,
-             month: t.month,
-             temp_spread: t.max_temp - t.min_temp)
 
 const tokens = temp_reader(t)
 const temp = strings_to_temp(tokens)
@@ -44,7 +16,7 @@ suite "a temperature record reader":
   test "max_temp is 8.9":
     check(tokens[2] == "8.9")
 
-suite "a Temperature record object":
+suite "a temperature record object":
   test "can be created from a seq of strings":
     check(temp.year == 1948)
   test "month is 1":
